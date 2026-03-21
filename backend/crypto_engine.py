f655dcd66debf394b2b4dd931d0fa9a05958d2ba
NathanielManoj <nathaniel.manoj@ontariotechu.net>
2026-03-21 14:17:02 -0400

    [crypto] made the derive key function]

--- backend/crypto_engine.py
+++ backend/crypto_engine.py
@@ -1,6 +1,23 @@
+# crypto_engine.py
+# Nathaniel Manoj 100817043
+# SOFE 4840U - Computer Security
+# This module provides functions for generating salts, deriving keys from passwords, and encrypting/decrypting data using AES-GCM.
+# It uses the `cryptography` library for AES-GCM encryption and decryption, and the `hashlib` library for key derivation using PBKDF2.
 import os
 import hashlib
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-
+# The `generate_salt` function generates a random salt using `os.urandom`.
 def generate_salt():
     return os.urandom(16)
+
+# The `derive_key` function derives a key from a password and salt using PBKDF2 with HMAC-SHA256, 600,000 iterations, and a key length of 32 bytes (256 bits).
+def derive_key(password, salt):
+    return hashlib.pbkdf2_hmac(
+    'sha256',
+    password.encode('utf-8'),
+    salt,
+    600000,
+    dklen=32
+    )
+
+
